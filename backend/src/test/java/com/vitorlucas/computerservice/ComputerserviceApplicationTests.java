51ffa264106f6175c861864cca754ec18408eebb
Vitor Lucas <vitorluc.2013@gmail.com>
2021-04-11 01:13:04 -0300

    Backend project created

---
+++ backend/src/test/java/com/vitorlucas/computerservice/ComputerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.computerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
