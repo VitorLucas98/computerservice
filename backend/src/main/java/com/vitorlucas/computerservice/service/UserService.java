891579f424ea47506e0a5b9031c87c4c165ba8ca
Vitor Lucas <vitorluc.2013@gmail.com>
2021-04-14 12:19:58 -0300

    Add put user

--- backend/src/main/java/com/vitorlucas/computerservice/service/UserService.java
+++ backend/src/main/java/com/vitorlucas/computerservice/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,27 +13,27 @@ import org.springframework.transaction.annotation.Transactional;
 import com.vitorlucas.computerservice.dto.UserDTO;
 import com.vitorlucas.computerservice.entities.User;
 import com.vitorlucas.computerservice.repositories.UserRepository;
-import com.vitorlucas.computerservice.service.exceptions.EntityNotFoundException;
+import com.vitorlucas.computerservice.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
-	private UserRepository repository; 
-	
+	private UserRepository repository;
+
 	@Transactional(readOnly = true)
-	public List<UserDTO> findAll(){
-		List<User> list =repository.findAll();
+	public List<UserDTO> findAll() {
+		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional(readOnly = true)
-	public UserDTO findById(Long id){
+	public UserDTO findById(Long id) {
 		Optional<User> entity = repository.findById(id);
-		User user = entity.orElseThrow(() ->new EntityNotFoundException("Entity not found"));
+		User user = entity.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(user);
 	}
-	
+
 	@Transactional
 	public UserDTO insert(UserDTO dto) {
 		User user = new User(null, dto.getName(), dto.getEmail(), dto.getPassword());
@@ -39,4 +41,19 @@ public class UserService {
 		user = repository.save(user);
 		return new UserDTO(user);
 	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setEmail(dto.getEmail());
+			entity.setPassword(dto.getPassword());
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found" + id);
+		}
+	}
+
 }
