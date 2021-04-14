9b18af548307b45c512320fcb5e59587a15004a3
Vitor Lucas <vitorluc.2013@gmail.com>
2021-04-14 15:06:27 -0300

    Add delete user

--- backend/src/main/java/com/vitorlucas/computerservice/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/vitorlucas/computerservice/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.vitorlucas.computerservice.service.exceptions.DatabaseException;
 import com.vitorlucas.computerservice.service.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +20,12 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(),"Resource not found", 
 				e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request){
+		StandardError err = new StandardError(Instant.now(), HttpStatus.BAD_REQUEST.value(),"Database exception", 
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
